87422ef3b981638b218ee2b348e3b05f3ab046fb
Jirka <vrbka.jirka@gmail.com>
2020-03-24 14:54:04 +0100

    Extracted method and formatting

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -15,21 +15,26 @@ namespace Tennis
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
-            {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-            }
             
             if (p1 == p2)
-
                 return "Deuce";
+            
+            
+            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            {
+                return GetScoreWhenUnderFour();
+            }
 
-            s = p1 > p2 ? p1N : p2N;
+            var s = p1 > p2 ? p1N : p2N;
             return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
             
+        }
+
+        private string GetScoreWhenUnderFour()
+        {
+            string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
+            var s = p[p1];
+            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
         }
 
         public void WonPoint(string playerName)
