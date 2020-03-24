956e5dd74d1a5dad51bbea372119e658801d4c6e
Jirka <vrbka.jirka@gmail.com>
2020-03-24 14:47:27 +0100

    Game3 Formating

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -22,13 +22,14 @@ namespace Tennis
                 s = p[p1];
                 return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
             }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+            
+            if (p1 == p2)
+
+                return "Deuce";
+
+            s = p1 > p2 ? p1N : p2N;
+            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            
         }
 
         public void WonPoint(string playerName)
