d83a97499dbdf99a471bf8a1dee7794d599c20d1
Jirka <vrbka.jirka@gmail.com>
2020-03-24 13:50:17 +0100

    returns in ifs

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -26,23 +26,21 @@ namespace Tennis
             string score = "";
             var tempScore = 0;
             if (_player1Score == _player2Score)
-            {
                 return GetScoreWhenEqual();
-            }
-            else
+            
             if (_player1Score >= 4 || _player2Score >= 4)
             {
                 var minusResult = _player1Score - _player2Score;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult == 1) 
+                    return "Advantage player1";
+                if (minusResult == -1) 
+                    return "Advantage player2";
+                if (minusResult >= 2) 
+                    return "Win for player1";
+                return "Win for player2";
             }
-            else
-            {
-                return $"{ScoreToString(_player1Score)}-{ScoreToString(_player2Score)}";
-            }
-            return score;
+                
+            return $"{ScoreToString(_player1Score)}-{ScoreToString(_player2Score)}";
         }
 
         private string GetScoreWhenEqual()
