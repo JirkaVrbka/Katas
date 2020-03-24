7953216e40bea8efdb5c0c2d17976da3ce2aac22
Jirka <vrbka.jirka@gmail.com>
2020-03-24 14:18:26 +0100

    direct return in ifs

--- Tennis/TennisGame2.cs
+++ Tennis/TennisGame2.cs
@@ -26,36 +26,36 @@ namespace Tennis
             
             if (p1point == p2point && p1point < 3)
             {
-                score = $"{p1res}-All";
+                return $"{p1res}-All";
             }
             if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+                return "Deuce";
 
             if (p1point > 0 && p2point == 0
             || p2point > 0 && p1point == 0
             || p1point > p2point && p1point < 4
             || p2point > p1point && p2point < 4)
             {
-                score = $"{p1res}-{p2res}";
+                return $"{p1res}-{p2res}";
             }
 
             if (p1point > p2point && p2point >= 3)
             {
-                score = "Advantage player1";
+                return "Advantage player1";
             }
 
             if (p2point > p1point && p1point >= 3)
             {
-                score = "Advantage player2";
+                return "Advantage player2";
             }
 
             if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
             {
-                score = "Win for player1";
+                return "Win for player1";
             }
             if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
-                score = "Win for player2";
+                return "Win for player2";
             }
             return score;
         }
